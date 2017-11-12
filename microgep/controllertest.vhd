7b3556d44a07032e003ae8b690c1a2ea222029d0
koletom <kole_tom@hotmail.com>
2017-11-12 20:21:38 +0100

    up controllertest and asm compiler

---
+++ microgep/controllertest.vhd
@@ -0,0 +1,51 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.helper.all;
+
+entity controllertest is port(
+	OUTData: out std_logic_vector(MEMBitsWidth-1 downto 0)
+);
+end entity;
+
+architecture BEH of controllertest is
+component controller is 
+	port (  clk:std_logic;   
+		rst:std_logic;   
+	        OUTData: out std_logic_vector(MEMBitsWidth-1 downto 0)
+	);
+end component;
+signal lclk:std_logic:='0';
+signal lrst:std_logic:='0';
+signal lOUTData: std_logic_vector(MEMBitsWidth-1 downto 0):=(others=>'0');
+
+begin
+
+controllermap:controller port map(lclk,lrst,lOUTData);
+
+process
+begin
+	lrst<='0';
+	for idx in 0 to 500 loop
+ 		lclk <= '1';
+		OUTData <= lOUTData;
+		wait for 10 ps;
+
+		lclk <= '0';
+		OUTData <= lOUTData;
+		wait for 10 ps;
+	end loop;	
+
+	lclk <= '1';
+	lrst<='1';
+	OUTData <= lOUTData;
+	wait for 10 ps;
+
+	lclk <= '0';
+	OUTData <= lOUTData;
+	wait for 10 ps;
+
+end process;
+end BEH;
+
